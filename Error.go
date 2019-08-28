8b64f30741c835085204ea1f84a353410e474caa
Gerald <sundaywind2004@gmail.com>
2019-08-28 22:42:02 +0800

    add type GError

---
+++ Error.go
@@ -0,0 +1,34 @@
+package fridago
+
+/*
+ #include "frida-core.h"
+*/
+import "C"
+// Errors
+// AddressInUseError
+// ExecutableNotFoundError
+// ExecutableNotSupportedError
+// InvalidArgumentError
+// InvalidOperationError
+// NotSupportedError
+// PermissionDeniedError
+// ProcessNotFoundError
+// ProcessNotRespondingError
+// ProtocolError
+// ServerNotRunningError
+// TimedOutError
+// TransportError
+
+type GError struct {
+	Msg string
+	Code int
+}
+
+func (err *GError) Error() string {
+	return err.Msg
+}
+
+func (err *GError) New(gerr *C.GError) {
+	err.Msg = C.GoString(gerr.message)
+	err.Code = int(gerr.code)
+}
